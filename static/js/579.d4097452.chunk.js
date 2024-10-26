20a079dd05df14f6d90088892b7a1de606acf3cd
Nishansha Negi <nishanegi16112001@gmail.com>
2024-10-26 21:54:35 +0530

    Updates

---
+++ static/js/579.d4097452.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_website=self.webpackChunkpersonal_website||[]).push([[579],{4579:(e,s,o)=>{o.r(s),o.d(s,{default:()=>r});o(5043);const a="NotFound_container__lLoRs",n="NotFound_title__2lpva",t="NotFound_message__0wOpy";var l=o(579);const r=()=>(0,l.jsxs)("div",{className:a,children:[(0,l.jsx)("h1",{className:n,children:"404 - Page Not Found"}),(0,l.jsx)("p",{className:t,children:"Sorry, the page you are looking for does not exist."})]})}}]);
+//# sourceMappingURL=579.d4097452.chunk.js.map
